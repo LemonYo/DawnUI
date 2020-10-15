374190a91b1e70c2d97e7164269ec5926fe9822e
LemonYo <1411577366@qq.com>
2020-10-15 10:08:12 +0800

    更新react 热更新

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import { hot } from 'react-hot-loader/root'
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import DemoRoutes from "./demos/Routes"
 import DocLayout from "./docs/Layout"
 import './app.scss'
 import Index from './Index'
-
 
 function App() {
   return (
@@ -20,4 +20,4 @@ function App() {
 }
 
 
-export default App
+export default hot(App)
